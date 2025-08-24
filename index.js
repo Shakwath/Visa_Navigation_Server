ccdf0a34b03c815f5d0b5d0b55e30b1c717dce0a
shakwath <shakaawathhossain0@gmail.com>
2025-08-25 02:06:31 +0600

    User name and pass secure & use .env,gitignore for security

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-//xWXgFbMyiAxzV2Bi
-//visa-navigation
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b73grgu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = "mongodb+srv://visa-navigation:xWXgFbMyiAxzV2Bi@cluster0.b73grgu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
